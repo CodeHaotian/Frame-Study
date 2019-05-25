599791b7af66d870227008b07b7e38d6b48134a4
admin <2056423011@qq.com>
2019-05-25 23:02:21 +0800

    Spring学习

---
+++ Spring_Study/src/test/java/SpringTest05.java
@@ -0,0 +1,31 @@
+import com.spring.study.factory.MyBeanFactory;
+import com.spring.study.model.User;
+import com.spring.study.service.UserService;
+import com.spring.study.service.impl.StudentService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @Author：Haotian
+ * @Date：2019/5/24 21:43
+ */
+public class SpringTest05 {
+
+    @Test
+    public void test1() {
+        //获取Spring容器中代理对象
+        ApplicationContext context = new ClassPathXmlApplicationContext( "beans6.xml" );
+
+        UserService userService = (UserService) context.getBean( "userService" );
+        User user = new User();
+        user.setUsername( "测试" );
+
+        userService.add( user );
+
+        StudentService ss = (StudentService) context.getBean( "studentService" );
+        ss.add();
+    }
+
+
+}
