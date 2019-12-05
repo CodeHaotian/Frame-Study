b65e719914950c68c153bb938a5a1b2bb305bd69
Rainbow Cat <2056423011@qq.com>
2019-12-05 14:02:35 +0800

    内容更新

--- Spring_Study/src/main/java/com/spring/study/service/impl/GrilServiceImpl.java
+++ Spring_Study/src/main/java/com/spring/study/service/impl/GrilServiceImpl.java
@@ -4,6 +4,7 @@ import com.spring.study.dao.GrilDao;
 import com.spring.study.domain.Gril;
 import com.spring.study.service.GrilService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ import java.util.List;
  * @Date: 2019/11/30 20:03
  * @Description: 用户操作方法实现
  */
+@Transactional(rollbackFor = Exception.class)
 @Service
 public class GrilServiceImpl implements GrilService {
     private final GrilDao grilDao;
