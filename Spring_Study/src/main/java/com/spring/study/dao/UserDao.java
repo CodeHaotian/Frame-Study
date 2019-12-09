be0d46160ca05ee05dd3a9b8c585d0fe625418bf
Rainbow Cat <2056423011@qq.com>
2019-12-09 20:20:13 +0800

    内容更新

--- Spring_Study/src/main/java/com/spring/study/dao/UserDao.java
+++ Spring_Study/src/main/java/com/spring/study/dao/UserDao.java
@@ -2,6 +2,8 @@ package com.spring.study.dao;
 
 import com.spring.study.model.User;
 
+import java.util.List;
+
 /**
  * @Author：Haotian
  * @Date：2019/5/25 16:25
@@ -12,4 +14,5 @@ public interface UserDao {
      * @param user 用户
      */
     void add(User user);
+
 }
