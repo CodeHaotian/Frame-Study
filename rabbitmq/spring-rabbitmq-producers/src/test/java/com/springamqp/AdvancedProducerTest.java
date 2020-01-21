0dfbbf4e21a0889ca3d8bd2f9dbc627b63321188
Rainbow Cat <2056423011@qq.com>
2020-01-21 17:07:13 +0800

    mq 进阶操作，消息可靠性保证，回退模式

--- rabbitmq/spring-rabbitmq-producers/src/test/java/com/springamqp/AdvancedProducerTest.java
+++ rabbitmq/spring-rabbitmq-producers/src/test/java/com/springamqp/AdvancedProducerTest.java
@@ -2,6 +2,7 @@ package com.springamqp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +33,6 @@ public class AdvancedProducerTest {
         //2.定义回调
         rabbitTemplate.setConfirmCallback( new RabbitTemplate.ConfirmCallback() {
             /**
-             *
              * @param correlationData 相关配置信息
              * @param ack exchange交换机 是否成功收到了消息。true 成功，false代表失败
              * @param cause 失败原因
@@ -52,5 +52,45 @@ public class AdvancedProducerTest {
         //3. 发送消息
         rabbitTemplate.convertAndSend( "test_exchange_confirm", "confirm", "message confirm...." );
         rabbitTemplate.convertAndSend( "test_exchange_confirm", "confirm", "message confirm...." );
+    }
+
+    /**
+     * 回退模式： 当消息发送给Exchange后，Exchange路由到Queue失败时，才会执行 ReturnCallBack
+     * 步骤：
+     * 1. 开启回退模式:publisher-returns="true"
+     * 2. 设置ReturnCallBack
+     * <pre>
+     * 3. 设置Exchange处理消息的模式：
+     *     1. 如果消息没有路由到Queue，则丢弃消息（默认）
+     *     2. 如果消息没有路由到Queue，返回给消息发送方ReturnCallBack
+     * </pre>
+     */
+    @Test
+    public void testReturn() {
+        //3.设置交换机处理失败消息的模式
+        rabbitTemplate.setMandatory( true );
+
+        //2.设置ReturnCallBack
+        rabbitTemplate.setReturnCallback( new RabbitTemplate.ReturnCallback() {
+            /**
+             * @param message 消息对象
+             * @param replyCode 错误码
+             * @param replyText 错误信息
+             * @param exchange  交换机
+             * @param routingKey 路由键
+             */
+            @Override
+            public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+                System.out.println( "return 执行了...." );
+                System.out.println( message );
+                System.out.println( replyCode );
+                System.out.println( replyText );
+                System.out.println( exchange );
+                System.out.println( routingKey );
+                //回退处理
+            }
+        } );
+        //3. 发送消息
+        rabbitTemplate.convertAndSend( "test_exchange_confirm", "abc", "message confirm...." );
     }
 }
